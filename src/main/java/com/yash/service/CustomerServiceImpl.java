51e93fb726c01d2cd0a058cb9f2e519bc168d96a
Yashpal <yashpal@nagarro.com>
2019-03-28 14:28:50 +0530

    - Added application Context - Added Bean in context - Replaced 'new' with appContext - Acheived abstraction using Setter Injecttion

--- src/main/java/com/yash/service/CustomerServiceImpl.java
+++ src/main/java/com/yash/service/CustomerServiceImpl.java
@@ -2,14 +2,18 @@ package com.yash.service;
 
 import com.yash.model.Customer;
 import com.yash.repository.HibernateCustomerRepo;
-import com.yash.repository.HibernateCustomerRepoImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
-    private HibernateCustomerRepo repo = new HibernateCustomerRepoImpl();
+
+    private HibernateCustomerRepo customerRepo;
+
+    public void setCustomerRepo(HibernateCustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
 
     public List<Customer> findAll() {
-        return repo.findAll();
+        return customerRepo.findAll();
     }
 }
