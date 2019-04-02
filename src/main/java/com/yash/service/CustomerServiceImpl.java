e23f2a346d12f64128015d4dc40e8978bc4eec36
Yashpal <yashpal@nagarro.com>
2019-04-02 14:16:40 +0530

    Introduced Spring Singleton Pattern using Java Conf

--- src/main/java/com/yash/service/CustomerServiceImpl.java
+++ src/main/java/com/yash/service/CustomerServiceImpl.java
@@ -3,13 +3,14 @@ package com.yash.service;
 import com.yash.model.Customer;
 import com.yash.repository.HibernateCustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("customerService")
-@Scope("singleton")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
