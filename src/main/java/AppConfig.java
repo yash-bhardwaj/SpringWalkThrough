fb03197054de57f25fab966e159c471cdb245698
Yashpal <yashpal@nagarro.com>
2019-04-02 14:09:05 +0530

    Introduced JAVA Setter Autowired injection using Java conf after removing all Beans Config boiler plate removed

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,24 +1,6 @@
-import com.yash.repository.HibernateCustomerRepo;
-import com.yash.repository.HibernateCustomerRepoImpl;
-import com.yash.service.CustomerService;
-import com.yash.service.CustomerServiceImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan({"com.yash"})
-public class AppConfig {
-
-    /*@Bean(name = "customerService")
-    public CustomerService getCustomerService() {
-        CustomerServiceImpl service = new CustomerServiceImpl();//getCustomerRepo());
-        //service.setCustomerRepo(getCustomerRepo());
-        return service;
-    }*/
-
-    /*@Bean(name = "customerRepo")
-    public HibernateCustomerRepo getCustomerRepo(){
-        return new HibernateCustomerRepoImpl();
-    }*/
-}
+public class AppConfig { }
--- src/main/java/com/yash/service/CustomerServiceImpl.java
+++ src/main/java/com/yash/service/CustomerServiceImpl.java
@@ -3,15 +3,12 @@ package com.yash.service;
 import com.yash.model.Customer;
 import com.yash.repository.HibernateCustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
-
-    public CustomerServiceImpl (){}
 
     @Autowired
     public void setCustomerRepo(HibernateCustomerRepo customerRepo) {
@@ -20,11 +17,6 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
     private HibernateCustomerRepo customerRepo;
-
-    public  CustomerServiceImpl(HibernateCustomerRepo customerRepo) {
-        System.out.println("Constructor Inject in Java Conf");
-        this.customerRepo = customerRepo;
-    }
 
     public List<Customer> findAll() {
         return customerRepo.findAll();
