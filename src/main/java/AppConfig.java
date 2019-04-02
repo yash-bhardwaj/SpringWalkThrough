f8ae8627a7a03b42507219e37b51751d5d9a23db
Yashpal <yashpal@nagarro.com>
2019-04-02 14:47:23 +0530

    Introduced Property configuration using annotations in AppConfig

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,6 +1,15 @@
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"com.yash"})
-public class AppConfig { }
+@PropertySource("app.properties")
+public class AppConfig {
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
--- src/main/java/com/yash/repository/HibernateCustomerRepoImpl.java
+++ src/main/java/com/yash/repository/HibernateCustomerRepoImpl.java
@@ -1,6 +1,7 @@
 package com.yash.repository;
 
 import com.yash.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,7 +10,10 @@ import java.util.List;
 @Repository("customerRepo")
 public class HibernateCustomerRepoImpl implements HibernateCustomerRepo {
 
+    @Value("${dbUsername}")
+    private String dbUsername;
     public List<Customer> findAll() {
+        System.out.println("S: "+ dbUsername);
         List<Customer> customers = new ArrayList();
         Customer customer = new Customer() {
             {
