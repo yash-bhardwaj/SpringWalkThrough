1cfb9f54fc2478e9137ddc150dda5164e96c39bc
Yashpal <yashpal@nagarro.com>
2020-01-22 17:26:15 +0530

    Process String replacement

--- src/main/java/com/yash/spring/SpringBootAppApplication.java
+++ src/main/java/com/yash/spring/SpringBootAppApplication.java
@@ -5,7 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.util.StringUtils;
 
 //@SpringBootApplication
 public class SpringBootAppApplication {
@@ -14,6 +19,39 @@ public class SpringBootAppApplication {
         //		SpringApplication.run(SpringBootAppApplication.class, args);
         //	}
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        processStringProblem(br);
+        //processSeriesProblem(br);
+    }
+
+    private static void processStringProblem(final BufferedReader br) throws IOException {
+        List<Integer> n_q = Arrays.asList(br.readLine().split(" ")).stream().map(Integer::parseInt).collect(Collectors.toList());
+        final int n = n_q.get(0);
+        int q = n_q.get(1);
+        String a = br.readLine();
+        String b = br.readLine();
+        final Map<Integer, Character> characterMap = new HashMap<>();
+        final char[] chars = b.toCharArray();
+        for (int j = 0; j < chars.length; j++) {
+            characterMap.put(j, chars[j]);
+        }
+        String i;
+        int queryNum;
+        while ((i = br.readLine()) != null) {
+            queryNum = Integer.parseInt(i)-1;
+            if (queryNum + 1 > n ) throw new RuntimeException("ArrayIndexOutOfBounds");
+            if (queryNum <= b.length()){
+                characterMap.put(queryNum, '1');
+            }
+            b = characterMap.values().stream().map(String::valueOf).collect(Collectors.joining(""));
+            if (a.equals(b)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+    private static void processSeriesProblem(final BufferedReader br) throws IOException {
         int T = Integer.parseInt(br.readLine());
         final List<Integer> series = getSeries(Math.pow(10, T));
         while (T >= 0) {
