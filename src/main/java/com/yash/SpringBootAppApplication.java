1a921e7f02eb39c5ed6482a973f0b92532be9260
Yashpal <yashpal@nagarro.com>
2020-01-24 21:41:39 +0530

    shinchainProblem

--- src/main/java/com/yash/SpringBootAppApplication.java
+++ src/main/java/com/yash/SpringBootAppApplication.java
@@ -10,22 +10,37 @@ public class SpringBootAppApplication {
 	public static void main(String[] args) throws IOException {
 //		SpringApplication.run(SpringBootAppApplication.class, args);
 		final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		final String line = br.readLine();
+		String[] A = line.split(" ");
 		int i = Integer.parseInt(br.readLine());
 		while(i > 0) {
 			--i;
-			final String line = br.readLine();
-			System.out.println(countNoVowelChars(line) + "/" + line.length());
+			final String s = br.readLine();
+			printOutput(s, A);
 		}
 	}
 
-	private static long countNoVowelChars(final String line) {
-		return line
-				.replace("www.", "")
-				.replace("a", "")
-				.replace("e", "")
-				.replace("i", "")
-				.replace("o", "")
-				.replace("u", "").length() + 1;
-	}
+	private static void printOutput(final String query, final String[] A) {
+		final String[] B = query.split(" ");
+		int l = Integer.parseInt(B[0]);
+		final int r = Integer.parseInt(B[1]);
+		final int X = Integer.parseInt(B[2]);
 
+		int xor;
+		final int[] range = new int[r];
+		int temp = Integer.MAX_VALUE;
+		int Z = 0;
+		for (int i = l; i <= r; i++) {
+			Z = Integer.parseInt(A[i-1]);
+			range[i-1] = Z;
+			xor = Z ^ X;
+			if (temp > xor) temp = xor;
+		}
+		int count = 0;
+		for (int i=0; i<range.length;i++) {
+			if (temp < Z && range[i] == Z) count++;
+		}
+		System.out.println(Z + " " + count);
+	}
 }
