c2a7e6337c2eb9aa4de591fb4b86352b167e6c28
Yashpal <yashpal@nagarro.com>
2020-01-24 12:56:23 +0530

    Solution

--- src/main/java/com/yash/SpringBootAppApplication.java
+++ src/main/java/com/yash/SpringBootAppApplication.java
@@ -1,13 +1,28 @@
 package com.yash;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringBootAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAppApplication.class, args);
+	public static void main(String[] args) throws IOException {
+//		SpringApplication.run(SpringBootAppApplication.class, args);
+		final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		final String string = br.readLine();
+		int i = 0;
+		final int length = string.length();
+		System.out.print(getAllEvens(string));
+		while(i < length) {
+			++i;
+			System.out.print(" ");
+			System.out.print(getAllEvens(string.substring(i, length)));
+		}
+	}
+
+	private static long getAllEvens(final String substring) {
+		return substring.chars().mapToObj(e -> (char) e).filter(c -> c%2 == 0).count();
 	}
 
 }
